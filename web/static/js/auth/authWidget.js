29a880f872ea1d8abda973a707369729171bb791
Stéphane Bisinger <stephane.bisinger@renuo.ch>
2017-05-12 15:39:04 +0200

    Redirect to Google to get start authentication

--- web/static/js/auth/authWidget.js
+++ web/static/js/auth/authWidget.js
@@ -10,6 +10,10 @@ class AuthWidget extends React.Component {
 
   }
 
+  redirectToGoogle() {
+    window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?scope=openid%20email%20profile&redirect_uri=http%3A%2F%2Flocalhost%3A4000%2Fauth%2Fgoogle%2Fcallback&response_type=code&client_id=330918691596-4jqcn7tdjv6kk8ub8qmg7le0mfu5b9t1.apps.googleusercontent.com'
+  }
+
   render() {
     if (this.props.user) {
       return (
@@ -17,7 +21,7 @@ class AuthWidget extends React.Component {
       );
     } else {
       return (
-        <Button className="btn-outline-primary">Login with Google</Button>
+        <Button className="btn-outline-primary" onClick={ this.redirectToGoogle }>Login with Google</Button>
       );
     }
   }
