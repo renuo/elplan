eb7773565c1af0ea2b4410eae7678b9adf4a0a8d
Stéphane Bisinger <stephane.bisinger@renuo.ch>
2017-06-12 17:27:41 +0200

    Import specific widget to reduce build size

--- web/static/js/auth/authWidget.js
+++ web/static/js/auth/authWidget.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap/lib/Button'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 
